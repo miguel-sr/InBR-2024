528bfb407beb2f54705c428551a512bfeed3b873
Fernando <fehdacostaquintanilhafilho@gmail.com>
2023-06-09 09:56:30 -0300

    #8 Instanciar os 4 motores de locomoção

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,9 +4,22 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
 
 public class DriveSubsystem extends SubsystemBase {
+
+  private final WPI_VictorSPX leftFrontMotor = new WPI_VictorSPX(DriveConstants.kLeftFrontMotorPort);
+
+  private final  WPI_VictorSPX leftRearMotor = new WPI_VictorSPX(DriveConstants.kLeftRearMotorPort);
+
+  private final WPI_VictorSPX rightFrontMotor = new WPI_VictorSPX(DriveConstants.kRightFrontMotorPort);
+
+  private final WPI_VictorSPX RightRearMotor = new WPI_VictorSPX(DriveConstants.kLeftRearMotorPort);
+
+  
   /** Creates a new DriveSubsystem. */
   public DriveSubsystem() {}
 
