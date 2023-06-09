a76bef5ed6c5d5d9977f1190ee65281e3385395a
Fernando <fehdacostaquintanilhafilho@gmail.com>
2023-06-09 09:41:57 -0300

    #8 Criar o arquivo base do subsistema

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,10 +13,6 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
-
   public static class DriveConstants {
     public static final int kLeftFrontMotorPort = 1;
     public static final int kLeftRearMotorPort = 2;
